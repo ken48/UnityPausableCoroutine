99b0117cfa44c9b6fbd04657d2e5079c30cf0a52
Kirill Belonogov <kirill.belonogov@gmail.com>
2019-09-25 09:00:21 +0300

    Add files via upload

---
+++ Assets/PausableCoroutine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausableCoroutine : IEnumerator
+{
+    public object Current => _isPaused ? null : GetCurrentEnumerator().Current;
+
+    readonly MonoBehaviour _coHolder;
+    readonly Stack<IEnumerator> _coStack;
+    Coroutine _unityCo;
+    bool _isPaused;
+
+    public PausableCoroutine(MonoBehaviour coHolder, IEnumerator co)
+    {
+        _coHolder = coHolder;
+        _coStack = new Stack<IEnumerator>();
+        _coStack.Push(co);
+
+        _unityCo = _coHolder.StartCoroutine(this);
+    }
+
+    public void Stop()
+    {
+        if (_unityCo == null)
+            return;
+
+        _coHolder.StopCoroutine(_unityCo);
+        _unityCo = null;
+    }
+
+    public void SetPaused(bool isPaused)
+    {
+        _isPaused = isPaused;
+    }
+
+    public bool MoveNext()
+    {
+        if (_isPaused)
+            return true;
+
+        bool moveNext = GetCurrentEnumerator().MoveNext();
+        if (moveNext)
+        {
+            var current = GetCurrentEnumerator().Current;
+            if (current is IEnumerator curCo && curCo != GetCurrentEnumerator())
+                _coStack.Push(curCo);
+        }
+        else
+        {
+            _coStack.Pop();
+            moveNext = _coStack.Count > 0;
+        }
+
+        return moveNext;
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator GetCurrentEnumerator()
+    {
+        return _coStack.Peek();
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    PausableCoroutine _p;
+    bool _isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _p = new PausableCoroutine(this, Kuku());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isPaused = !_isPaused;
+            _p.SetPaused(_isPaused);
+        }
+    }
+
+    IEnumerator Kuku()
+    {
+        int x = 1;
+
+        while (true)
+        {
+            Debug.Log("-------- " + x++);
+
+            yield return new WaitForSeconds(0.5f);
+            yield return Foo();
+        }
+    }
+
+    IEnumerator Foo()
+    {
+        int x = 0;
+
+        while (x < 5)
+        {
+            Debug.Log("*** " + (x++));
+
+            yield return new WaitForSeconds(0.5f);
+            yield return new TestYield(0.5f);
+        }
+    }
+}
+
+class TestYield : CustomYieldInstruction
+{
+    readonly float _duration;
+    readonly Dictionary<int, float> _frameTimes;
+
+    public TestYield(float duration)
+    {
+        _duration = duration;
+        _frameTimes = new Dictionary<int, float>();
+    }
+
+    public override bool keepWaiting
+    {
+      get
+      {
+          _frameTimes[Time.frameCount] = Time.deltaTime;
+
+          Debug.Log(Time.frameCount + "TEST");
+
+          return GetTotalTime() < _duration;
+      }
+    }
+
+    float GetTotalTime()
+    {
+        float result = 0f;
+        foreach (float frameTime in _frameTimes.Values)
+            result += frameTime;
+        return result;
+    }
+}
